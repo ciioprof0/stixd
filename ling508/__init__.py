1682d9f258ea0f91b51f819d902041b2778252d3
ciioprof0 <116185051+ciioprof0@users.noreply.github.com>
2024-08-13 17:01:31 -0700

    Added test_end_to_end_local

--- ling508/__init__.py
+++ ling508/__init__.py
@@ -1,9 +1,16 @@
+"""This file is used to add the necessary directories to sys.path so that the
+modules in the app, db, model, tests, and web directories can be imported"""
+
 import sys
 import os
+
+# Append the project root directory to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
 
 # Append the necessary directories to sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, 'app'))
 sys.path.append(os.path.join(current_dir, 'db'))
 sys.path.append(os.path.join(current_dir, 'model'))
-sys.path.append(os.path.join(current_dir, 'test'))
+sys.path.append(os.path.join(current_dir, 'tests'))
--- ling508/api.py
+++ ling508/api.py
@@ -29,6 +29,7 @@ app = Flask(__name__, static_folder='D:/OneDrive/Code/hltms/stixd/ling508/web')
 
 @app.route('/ling508/web/stixd.html')
 def serve_form():
+    """Serves the form for importing Clex entries."""
     return app.send_static_file('stixd.html')
 
 
